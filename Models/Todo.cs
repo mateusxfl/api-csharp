99a456fd083caa6efae17a9af6d16e48806a1d24
Mateus Fonseca <mateus.xfl@gmail.com>
2022-05-31 11:07:49 -0300

    add Todo class

---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
