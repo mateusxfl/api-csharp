4c66ea502c85a643488126575039a8183378dbb5
Mateus Fonseca <mateus.xfl@gmail.com>
2022-06-01 10:17:43 -0300

    Revert "add route todos in TodoController"
    This reverts commit 69d7a9614fc30fe8750247dd9f6656ed3d27f606.

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using api.Models;
 
 namespace api.Controllers
 {
@@ -9,12 +7,5 @@ namespace api.Controllers
     public class TodoController : ControllerBase
     {
         //  O controlador dentro do MVC é o item que vai receber a requisição, manipular ela e devolvê-la para a tela.
-
-        // Action
-        [HttpGet]
-        [Route("todos")]
-        public List<Todo> Get(){
-            return new List<Todo>();
-        }
     }
 }
