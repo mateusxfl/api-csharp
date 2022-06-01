69d7a9614fc30fe8750247dd9f6656ed3d27f606
Mateus Fonseca <mateus.xfl@gmail.com>
2022-06-01 09:01:14 -0300

    add route todos in TodoController

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using api.Models;
 
 namespace api.Controllers
 {
@@ -7,5 +9,12 @@ namespace api.Controllers
     public class TodoController : ControllerBase
     {
         //  O controlador dentro do MVC é o item que vai receber a requisição, manipular ela e devolvê-la para a tela.
+
+        // Action
+        [HttpGet]
+        [Route("todos")]
+        public List<Todo> Get(){
+            return new List<Todo>();
+        }
     }
 }
